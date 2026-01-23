4bc735521e65bd8cdea825964a5aca2c48e841af
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-01-23 12:04:31 +0100

    Structure set in place, views, models, urls pattern all set-up, Nesxt stage is testing

---
+++ app/bookmark/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Opportunity(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    opportunity_type = models.CharField(max_length=100)  # e.g Internship, Scholarship, Job, Grant
+    description = models.TextField()
+    link = models.URLField(null=False, blank=False)
+    # location = models.CharField(max_length=255, null=True, blank=True)  # e.g Remote, On-site
+    posted_at = models.DateTimeField(auto_now_add=True)
+    is_open = models.BooleanField(default=True)
+    # created_by = models.ForeignKey('profiles.UserProfile', on_delete=models.CASCADE)  # Optional: Track who created the oppurtunity e.g Enabler
+
+    def __str__(self):
+        return self.title
+    
+class Bookmark(models.Model):
+    user = models.ForeignKey('profiles.UserProfile', on_delete=models.CASCADE)
+    opportunity = models.ForeignKey(Opportunity, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "opportunity")  # prevents duplicate saves
+    
+    def __str__(self):
+        return f"{self.user} → {self.opportunity.title}"
+    
+    
+
+ 
---
+++ app/bookmark/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+# write your ursls here
+
+urlpatterns = [
+    path("health/", health_check, name="bookmark-health-check"),
+
+    path('opportunities/', OpportunityView.as_view(), name='opportunity-list'), # list all opportunities
+    path('bookmarks/', BookmarkListCreateView.as_view(), name='bookmark-list'), # list and create bookmarks
+    path('bookmarks/<int:pk>/delete/', BookmarkDeleteView.as_view(), name='bookmark-delete'), # delete bookmark
+    path('opportunities/saved/', UserBookmarkListView.as_view(), name='user-bookmarks'), # list user's bookmarked opportunities
+]
