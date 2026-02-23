d8a0215686bbe83392e994c6b964c61b7fe3d3e3
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-02-23 07:00:26 +0100

    admin panel

--- app/opportunities/admin.py
+++ app/opportunities/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Opportunity
 
 # Register your models here.
+
+admin.site.site_header = "Afrivate Admin"
+admin.site.site_title = "Afrivate Admin Portal"
+admin.site.index_title = "Welcome to the Afrivate Admin Portal"
+
+admin.site.register(Opportunity)
