980e593bc2fc8cc8dd420284fd7fc4769c90362d
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-04-03 15:40:19 +0100

    Bookmark/urls.py  Changed the endpoints

--- app/bookmark/urls.py
+++ app/bookmark/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
 
         # bookmarks
     path('opportunities/saved/', BookmarkListCreateView.as_view(), name='bookmark-list'), # list and create bookmarks
-    path('bookmarks/<int:pk>/delete/', BookmarkDeleteView.as_view(), name='bookmark-delete'), # delete bookmark
+    path('opportunities/saved/<int:opportunity_id>/', BookmarkDeleteView.as_view(), name='bookmark-delete'), # delete bookmark
     # path('opportunities/saved/', UserBookmarkListView.as_view(), name='user-bookmarks'), # list user's bookmarked opportunities
     path('applicants/saved/', PathfinderBookmarkView.as_view(), name='pathfinder-bookmarks'), # list and create pathfinder bookmarks
-    path('applicants/saved/<int:pk>/delete/', PathfinderBookmarkDeleteView.as_view(), name='pathfinder-bookmark-delete'), # delete pathfinder bookmark
+    path('applicants/saved/<int:pathfinder_id>/', PathfinderBookmarkDeleteView.as_view(), name='pathfinder-bookmark-delete'), # delete pathfinder bookmark
 ]
